ed1f36e000b7a88cdbc8ec4f196168329b6a83dc
RomanSchmid <rrr.schmid@gmail.com>
2022-07-29 00:05:57 +0200

    Nav menu button rework

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,9 +1,14 @@
 function navMenuFunction() {
     let myLinks = document.getElementById("nav-menu");
-    console.log(myLinks.style.display);
+    let hamBtn = document.getElementById("hamburger-btn");
+    let xmarkBtn = document.getElementById("xmark-btn");
     if (myLinks.style.display === "block") {
         myLinks.style.display = "none";
+        hamBtn.style.display = "block"
+        xmarkBtn.style.display = "none";
     } else {
         myLinks.style.display = "block";
+        hamBtn.style.display = "none";
+        xmarkBtn.style.display = "block";
     }
 }
